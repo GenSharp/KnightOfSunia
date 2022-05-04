26982780128ad2b62ec709cecce1fdd055866ef7
GenSharp <lukamartan1@gmail.com>
2022-05-04 16:22:41 +0200

    Added basic health components

--- KnightOfSunia/Assets/Scripts/HealthBar.cs
+++ KnightOfSunia/Assets/Scripts/HealthBar.cs
@@ -2,11 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Health : MonoBehaviour
+public class HealthBar : MonoBehaviour
 {
-
-    public static int health = 100;
-
     // Start is called before the first frame update
     void Start()
     {
--- KnightOfSunia/Assets/Scripts/Player.cs
+++ KnightOfSunia/Assets/Scripts/Player.cs
@@ -18,6 +18,8 @@ public class Player : MonoBehaviour
     private Vector3 change;
     private Animator animator;
 
+    public static int health;
+
     // Start is called before the first frame update
     void Start()
     {
