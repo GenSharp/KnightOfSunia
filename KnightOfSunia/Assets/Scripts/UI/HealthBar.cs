294249ee99eea80338d5729d8d1ae3b88ad505ff
GenSharp <lukamartan1@gmail.com>
2022-05-05 19:43:43 +0200

    Added HealthBar to Player

--- KnightOfSunia/Assets/Scripts/UI/HealthBar.cs
+++ KnightOfSunia/Assets/Scripts/UI/HealthBar.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
+
+    public Slider slider;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,10 @@ public class HealthBar : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
     }
 }
