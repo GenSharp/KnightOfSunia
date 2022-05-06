b9f2edd8a9552dceea18f42190a0c5f1f0d006c6
GenSharp <lukamartan1@gmail.com>
2022-05-06 15:29:39 +0200

    Added random drop script

--- KnightOfSunia/Assets/Scripts/Player.cs
+++ KnightOfSunia/Assets/Scripts/Player.cs
@@ -36,7 +36,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)) //test izbrisati
         {
             health -= 25;
             healthBar.SetHealth(health);
