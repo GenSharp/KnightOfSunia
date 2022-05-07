a431cc3bb06b016fd18483f8a40f7d566c3a088b
GenSharp <lukamartan1@gmail.com>
2022-05-07 16:56:07 +0200

    Added a bunch of scripts
    Added Chest, Coin, Dialog and NPC.
    Need to test all of them.

---
+++ KnightOfSunia/Assets/Scripts/Chest.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+
+    public GameObject loot;
+    Animator animator;
+    bool isCollision = false;
+    bool isOpen = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && isCollision)
+        {
+            //animator.SetInteger("ChestAnim", 1);
+            var obj = Instantiate(loot, new Vector3(transform.position.x, transform.position.y + 1, 0), Quaternion.identity) as GameObject;
+            isCollision = false;
+            isOpen = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && !isOpen)
+        {
+            isCollision = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            isCollision = false;
+        }
+    }
+}
