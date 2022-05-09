26a4fc91faab4c12f7f8465907e53e8362e99a70
MrBadger9 <103524685+MrBadger9@users.noreply.github.com>
2022-05-09 22:34:44 +0200

    boss

--- KnightOfSunia/Assets/Scripts/Hostile NPC/MainBoss.cs
+++ KnightOfSunia/Assets/Scripts/Hostile NPC/MainBoss.cs
@@ -4,67 +4,58 @@ using UnityEngine;
 
 public class MainBoss : MonoBehaviour
 {
-
-    public int health;
-    public GameObject spawnEnemy;
-    public GameObject spawnLeuone;
-    public float spawningDelay;
-    public bool canSpawn = true;
-    public bool canSpawnLeuone = false;
-    public Animator anim;
-
+    public float health;
+    public float Cooldown;
+    public bool cooldown;
+    public GameObject gobo;
+    public int goblinspawn;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (canSpawn)
+        if(cooldown == true)
         {
-            StartCoroutine(Spawn());
+            StartCoroutine(spawn());
+        }
+        if(goblinspawn>=25)
+        {
+            Destroy(gameObject);
         }
 
-        if (health <= 50 && canSpawnLeuone)
-        {
-            canSpawnLeuone = true;
-            StartCoroutine(SpawnLeuone());
-        }
 
-        else if (health <= 0)
-        {
-            StartCoroutine(Death());
-        }
     }
 
-    public IEnumerator Spawn()
+
+    public IEnumerator spawn()
     {
-        canSpawn = false;
-        Instantiate(spawnEnemy, transform.position, Quaternion.identity);
-        Instantiate(spawnEnemy, transform.position, Quaternion.identity);
-        yield return new WaitForSeconds(spawningDelay);
-        canSpawn = true;
-    }
-
-    public IEnumerator SpawnLeuone()
-    {
-        Instantiate(spawnLeuone, transform.position, Quaternion.identity);
-        canSpawnLeuone = false;
-        yield return new WaitForEndOfFrame();
-    }
-
-    public IEnumerator Death()
-    {
-        Instantiate(spawnEnemy, transform.position, Quaternion.identity);
-        Instantiate(spawnEnemy, transform.position, Quaternion.identity);
-        Instantiate(spawnEnemy, transform.position, Quaternion.identity);
-        Instantiate(spawnEnemy, transform.position, Quaternion.identity);
-        Instantiate(spawnEnemy, transform.position, Quaternion.identity);
-        Instantiate(spawnEnemy, transform.position, Quaternion.identity);
-        Destroy(gameObject);
-        yield return new WaitForEndOfFrame();
+        Instantiate(gobo, transform.position, Quaternion.identity);
+        cooldown = false;
+        yield return new WaitForSeconds(Cooldown);
+        cooldown = true;
+        goblinspawn = goblinspawn + 1;
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
